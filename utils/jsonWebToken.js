e8796a3046d199198de1922272c47da05d500ca0
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-14 02:52:05 +0100

    :)

--- utils/jsonWebToken.js
+++ utils/jsonWebToken.js
@@ -1,8 +1,8 @@
 const jsonwebtoken = require("jsonwebtoken");
 
-function signJWT(id) {
+function createJWT(id) {
   //  jwt.sign(objPAYLOAD, secretKey(from.env), objOPTIONS);
   return jsonwebtoken.sign({ id: id }, process.env.JWT_KEY, { expiresIn: process.env.JWT_EXPIRATION });
 }
 
-module.exports = { signJWT };
+module.exports = { createJWT };

0564a75e220a62a0c303f9a2cd138ccbd248cc65
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-20 18:21:50 +0100

    + getReview (m/uten :id)

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,12 +1,15 @@
 const ModelReview = require("../models/ModelReview");
 
 async function getReviews(req, res, next) {
-  const reviews = await ModelReview.find();
-
   try {
+    const filter = req.params.id ? { tour: req.params.id } : {};
+
+    const reviews = await ModelReview.find(filter);
+
     res.status(200).send({
       status: "success",
-      data: { reviews: reviews },
+      results: reviews.length,
+      data: reviews,
     });
   } catch (error) {
     res.status(500).send(error);
@@ -15,22 +18,26 @@ async function getReviews(req, res, next) {
 
 async function postReview(req, res, next) {
   try {
-    const review = {
+    const newReview = {
       user: req.user._id,
       tour: req.params.id,
       review: req.body.review,
       rating: req.body.rating,
     };
 
-    await ModelReview.create(review);
+    await ModelReview.create(newReview);
 
     res.status(200).send({
       status: "success",
-      newReview: review,
+      data: newReview,
     });
   } catch (error) {
     res.status(500).send(error);
   }
 }
+
+function patchReview(req, res, next) {}
+
+function deleteReview(req, res, next) {}
 
 module.exports = { getReviews, postReview };

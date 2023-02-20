31e2edb191903872a6f52c36cc3f582896042c16
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-20 18:20:05 +0100

    fjernet console.log()

--- utils/JWT.js
+++ utils/JWT.js
@@ -1,7 +1,6 @@
 const jsonwebtoken = require("jsonwebtoken");
 
 function verifyJWT(tokenJWT) {
-  console.log(jsonwebtoken.verify(tokenJWT, process.env.JWT_KEY));
   return jsonwebtoken.verify(tokenJWT, process.env.JWT_KEY);
 }
 
@@ -9,7 +8,7 @@ function res_createSendCookieJWT(res, user, statusCode = 200) {
   const tokenJWT = jsonwebtoken.sign({ id: user._id }, process.env.JWT_KEY, { expiresIn: process.env.JWT_EXPIRATION });
 
   const cookieOptions = {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRATION * 5 * 60 * 1000), // 5 minuteer
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRATION * 60 * 60 * 1000), // 60 minutter (JWT_C_EX = 1spo)
     httpOnly: true,
   };
 

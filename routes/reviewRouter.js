b03f833461d62c8bc14d1fa6a85d13eb030109fb
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-20 18:21:14 +0100

    mergeParams fra /tours/:id/reviews -> routerReview

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -3,12 +3,14 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const reviewController = require("../controllers/reviewController");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true }); // ! merger params fra redirect: /api/v1/tours/:id/reviews
 
 // ROUTES //
 // ROUTES //
 
-router.route("/").get(authController.protectWithLogIn, reviewController.getReviews);
-router.route("/").post(authController.protectWithLogIn, authController.restrictTo("user"), reviewController.postReview);
+router
+  .route("/")
+  .get(reviewController.getReviews)
+  .post(authController.protectWithLogIn, authController.restrictTo("user"), reviewController.postReview);
 
 module.exports = router;
